7f295d02cd4b162f0e8ad02290849d6f4dc0e744
rahandhana-007 <boldscaper@gmail.com>
2026-07-26 21:06:03 +0700

    koneksi supabase dan tes tampil produk

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL=https://qpdogonbygaxkconbqbh.supabase.co,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_0RvDC63DrrQlyR1d5dub_w_s7RFJwH2
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   )
 }
